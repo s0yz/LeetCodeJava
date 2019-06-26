7948035a545a278f41346ed7e585b6c6b63c07fe
cedric.mariage <cedric.mariage@gft.com>
2019-06-26 07:17:55 -0400

    Update Main.java

--- LeetCode/src/main/Main.java
+++ LeetCode/src/main/Main.java
@@ -1,12 +1,36 @@
 package main;
+
+import algorithm.ContainsNearbyDuplicate;
 import algorithm.ReapeatedNTimes;
 
-public class Main {	
-	
+public class Main {
+
 	public static void main(String[] args) {
-		int[] a = { 1, 2, 3, 4, 5, 6, 6, 6, 6, 6 };
-		System.out.println(ReapeatedNTimes.repeatedNTimesV1(a));
-		System.out.println(ReapeatedNTimes.repeatedNTimesV2(a));
-		System.out.println(ReapeatedNTimes.repeatedNTimesV3(a));
+		containsNearbyDuplicate();
+	}
+
+	public static void printResult(int version, Object r, Object e) {
+		System.out.println("Version " + version + " actual: " + r + ", expected: " + e);
+	}
+			
+	public static void containsNearbyDuplicate() {
+		int[][] arrays = { { 1, 2, 3, 1 }, { 1, 0, 1, 1 }, { 1, 2, 3, 1, 2, 3 }, {1,2,3,1,2,3} };
+		int[] ks = { 3, 1, 2, 2 };
+		boolean[] expected = { true, true, false, false };
+		for (int i = 0; i < arrays.length; ++i) {
+			//printResult(1, ContainsNearbyDuplicate.v1(arrays[i], ks[i]), expected[i]);
+			//printResult(2, ContainsNearbyDuplicate.v2(arrays[i], ks[i]), expected[i]);
+			printResult(3, ContainsNearbyDuplicate.v3(arrays[i], ks[i]), expected[i]);
+		}
+	}
+
+	public static void repeatedNTimes() {
+		int[][] arrays = { { 1, 2, 3, 3 }, { 2, 1, 2, 5, 3, 2 }, { 5, 1, 5, 2, 5, 3, 5, 4 } };
+		int[] expected = { 3, 2, 5 };
+		for (int i = 0; i < arrays.length; ++i) {
+			printResult(1, ReapeatedNTimes.v1(arrays[i]), expected[i]);
+			printResult(2, ReapeatedNTimes.v2(arrays[i]), expected[i]);
+			printResult(3, ReapeatedNTimes.v3(arrays[i]), expected[i]);
+		}
 	}
 }
