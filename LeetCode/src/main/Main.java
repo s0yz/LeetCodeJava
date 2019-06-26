06cbeaa292979e2a17511f0fb07f400a25df0c56
cedric.mariage <cedric.mariage@gft.com>
2019-06-25 21:31:06 -0400

    Update Main.java

--- LeetCode/src/main/Main.java
+++ LeetCode/src/main/Main.java
@@ -1,8 +1,12 @@
 package main;
+import algorithm.ReapeatedNTimes;
 
 public class Main {	
 	
 	public static void main(String[] args) {
-		
+		int[] a = { 1, 2, 3, 4, 5, 6, 6, 6, 6, 6 };
+		System.out.println(ReapeatedNTimes.repeatedNTimesV1(a));
+		System.out.println(ReapeatedNTimes.repeatedNTimesV2(a));
+		System.out.println(ReapeatedNTimes.repeatedNTimesV3(a));
 	}
 }
