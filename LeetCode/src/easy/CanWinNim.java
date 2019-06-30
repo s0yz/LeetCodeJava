b38893abc1d924bf6dea4b7d89a76f4dbdc17fd3
cedric.mariage <cedric.mariage@gft.com>
2019-06-29 23:28:52 -0400

    Create CanWinNim.java

---
+++ LeetCode/src/easy/CanWinNim.java
@@ -0,0 +1,28 @@
+package easy;
+
+/**
+ * You are playing the following Nim Game with your friend: There is a heap of
+ * stones on the table, each time one of you take turns to remove 1 to 3 stones.
+ * The one who removes the last stone will be the winner. You will take the
+ * first turn to remove the stones.
+ * <p>
+ * Both of you are very clever and have optimal strategies for the game. Write a
+ * function to determine whether you can win the game given the number of stones
+ * in the heap.
+ * 
+ * @see <a href= "https://leetcode.com/problems/nim-game/">
+ *      Nim Game</a>
+ */
+public class CanWinNim {
+
+	/**
+	 * Mod 4 - Was easy !
+	 * 
+	 * Runtime: 0 ms, faster than 100.00%
+	 * <p>
+	 * Memory Usage: 33 MB, less than 5.10%
+	 */
+	public boolean v1(int n) {
+		return n % 4 != 0;
+	}
+}
